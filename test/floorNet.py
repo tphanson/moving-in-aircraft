ee13644e7937dba10973b1a6158d8e38fdec1920
tphanson <tphanson@kambria.io>
2020-11-03 15:45:10 +0700

    debug

--- test/floorNet.py
+++ test/floorNet.py
@@ -25,7 +25,7 @@ def detect_edge(_):
         blur = cv.GaussianBlur(gray, (11, 11), 0)
         canny = cv.Canny(blur, 50, 150)
         hough = cv.HoughLinesP(canny, 1, np.pi / 180,
-                               threshold=80, minLineLength=50, maxLineGap=20)
+                               threshold=60, minLineLength=50, maxLineGap=20)
         lines = np.reshape(np.squeeze(hough), (hough.shape[0], 2, 2))
         for (a, b) in lines:
             img = cv.line(img, (a[0], a[1]), (b[0], b[1]), RED)
